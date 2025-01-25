83344767040fff7caa0fe9eee84a007b8e522ec9
luciamam <lumanmba2023@gmail.com>
2025-01-25 20:38:50 +0100

    corregido el error  estaba renderizando la ruta /login en vez de rediregir, corregido

--- src/appy.py
+++ src/appy.py
@@ -25,36 +25,56 @@ mysql=MySQL(app)
 
 
 
+
+#RUTA RAIZ noa tiene que tiene que redigir a la ruta login de alli usamos el redirect 
 @app.route('/',methods=['GET'])
 def inicio():
-        return render_template('login.html')
+        return redirect(url_for('login'))
 
 
-@app.route('/login',methods=['POST'])
+
+
+#la ruta /login nos muestra el formulario (renderiza el template .Login) y a la vez procesamos los datos este template 
+# por eso usamos los dos metodos  GET Y POST 
+@app.route('/login',methods=['GET','POST'])
 def login():
-        email=request.form['email']
-        password=request.form['password']
-        print(email,password)
-        ## consultare la base de datos  si este usuario existe 
-        return "datos insertados"
+        #para recoger datos del formaulario 
+        if request.method=='POST':
+            email=request.form['email']
+            password=request.form['password']
+            print(email,password) 
+
+            #siempre a la conexion  a la base de datos usar el try 
+            try:
+                cur=mysql.connection.cursor()
+                print("dentro del trai",email,password)
+                cur.execute('SELECT * FROM usuarios WHERE email=%s and password=%s',(email,password))
+                user=cur.fetchone()
+                #print("usuario",user[0])
+            
+                if user:
+                    return redirect(url_for('miperfil'))
+                else:
+                    return "usuario no valido "
+                
+            except Exception as e:
+                return f"ha ocurrido {e}"  
+        else:
+            #no quiero salirme de la ruta 
+            return render_template('login.html')
 
 
 
+#ruta registrarse 
 @app.route('/registrarse',methods=['GET','POST'])
 def registro():
+    #si el metodo es POST insertamos datos los datos en la base de datos 
     if request.method=='POST':
         nombre=request.form['nombre']
         apellidos=request.form['apellidos']
         email=request.form['email']
         password=request.form['password']
-
-        print(nombre,apellidos,email,password)
-
-        #lo guardare en la base de datos  estos datos que recoja de  aqui  
-    
-         # return "metodo post " esta es la respuesta tipica de flask pero ahora nosotros le queremos  a enviar a una ruta perfil 
-
-
+        #print(nombre,apellidos,email,password)
         #creamos la conexion con la base de datos 
         cur=mysql.connection.cursor()
         cur.execute('Insert into usuarios(nombre,apellidos,email,password) values (%s,%s,%s,%s)',(nombre,apellidos,email,password))
@@ -63,9 +83,16 @@ def registro():
         return redirect(url_for('perfil',nombre=nombre))
     else:
         return render_template('Registrarse.html')
-        
-   
 
+
+
+#ruta perfil 
+@app.route('/miperfil')
+def miperfil():
+    return render_template ('Miperfil.html')
+
+
+#ruta 
 @app.route('/perfil/<string:nombre>',methods=['GET'])
 def perfil(nombre):
     return render_template('Perfil.html',nombre=nombre)
